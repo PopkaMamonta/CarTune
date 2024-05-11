58d58d20137c515913e3ab392b37843f4492c931
Artur <angelartur357@gmail.com>
2024-05-11 21:36:49 +0300

    CAR-29 <footer 1.0>

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =require('express')
+const exphbs=require('express-handlebars')
+const app=express()
+
+
+const hbs=exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views','views')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+const PORT=process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
